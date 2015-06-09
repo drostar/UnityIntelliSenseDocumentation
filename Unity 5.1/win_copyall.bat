9ee886af8db2b036523c48b9786fd0d747129d8c
matthew <matthew.drotar@gmail.com>
2015-06-09 11:33:14 -0600

    Added documentation for Unity 5.1.

---
+++ Unity 5.1/win_copyall.bat
@@ -0,0 +1,47 @@
+@echo off
+REM Copies all Unity XML Documentation files into the appropriate Unity 4.6 application sub-folders for the default installation location
+
+
+REM Copying to Program Files folder requires administrative privileges.
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+REM Copy to 5.X.X folders
+echo %cd%
+IF EXIST "C:\Program Files\Unity\Editor\Data\Managed" (
+	echo Copying Engine and Editor files.
+	COPY UnityEngine.xml "C:\Program Files\Unity\Editor\Data\Managed"
+	COPY UnityEditor.xml "C:\Program Files\Unity\Editor\Data\Managed"
+)
+
+echo Copying UI files
+COPY UnityEngine.UI.xml "C:\Program Files\Unity\Editor\Data\UnityExtensions\Unity\GUISystem"
+COPY UnityEditor.UI.xml "C:\Program Files\Unity\Editor\Data\UnityExtensions\Unity\GUISystem\Editor"
+
+ECHO DONE!
+pause
+
+
